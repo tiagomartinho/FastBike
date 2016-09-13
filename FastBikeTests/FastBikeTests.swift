dbd0ffb0775358717b7a6782898cdfb665bf30b6
tiagomartinho <tiago.martinho@xpeppers.com>
2016-09-13 13:08:03 +0200

    Initial Commit

---
+++ FastBikeTests/FastBikeTests.swift
@@ -0,0 +1,36 @@
+//
+//  FastBikeTests.swift
+//  FastBikeTests
+//
+//  Created by Tiago Martinho on 13/09/16.
+//  Copyright © 2016 tm. All rights reserved.
+//
+
+import XCTest
+@testable import FastBike
+
+class FastBikeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
