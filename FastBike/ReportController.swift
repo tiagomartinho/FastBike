a098fb38944ac9f413a959a7e4e61760f1947cdf
tiagomartinho <tiago.martinho@xpeppers.com>
2016-09-14 11:30:12 +0200

    pickes image using UIImagePickerController

--- FastBike/ReportController.swift
+++ FastBike/ReportController.swift
@@ -1,10 +1,17 @@
 import UIKit
 
-class ReportController: UITableViewController {
+class ReportController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var problemDescription: UITextField!
     
     @IBOutlet weak var photo: UIImageView!
+
+    let textFieldShouldReturn = TextFieldShouldReturn()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textFieldShouldReturn.addTextField(textField: problemDescription)
+    }
 
     @IBAction func cancel(_ sender: AnyObject) {
         dismiss(animated: true, completion: nil)
@@ -15,5 +22,29 @@ class ReportController: UITableViewController {
     }
 
     @IBAction func takePhoto(_ sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .camera
+        present(picker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            photo.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+class TextFieldShouldReturn: NSObject, UITextFieldDelegate {
+
+    func addTextField(textField:UITextField){
+        textField.delegate=self
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
