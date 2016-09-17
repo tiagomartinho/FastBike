fbfcf2904e44080de65f8a89d7293a741cb5cac6
tiagomartinho <tiago.martinho@xpeppers.com>
2016-09-17 15:21:23 +0200

    adds link to settings when user didn't allow to access posistion

--- FastBike/AlertUtilities.swift
+++ FastBike/AlertUtilities.swift
@@ -2,10 +2,15 @@ import UIKit
 
 class AlertUtilities {
     static func showErrorAlert(viewController: UIViewController) {
-        let alert = UIAlertController(title: "Mi spiace 🤗",
-                                      message: "Stai senza bici 🏃🏻",
+        let alert = UIAlertController(title: "Attenzione",
+                                      message: "Per usare l'applicazione devi permettere l'uso della tua posizione",
                                       preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok...", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Attiva posizione", style: .default, handler: { _ in
+            if let url = URL(string: UIApplicationOpenSettingsURLString) {
+                UIApplication.shared.open(url, options: [String:Any](), completionHandler: nil)
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Annulla", style: .cancel, handler: nil))
         viewController.present(alert, animated: true, completion: nil)
     }
 }
