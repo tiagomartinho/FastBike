7c03248f9f2a95222f8a8bd5a6534ea94a4bb198
MatteoPierro <pierromatteo@gmail.com>
2016-09-20 13:30:43 +0200

    refactoring BikeStationService

--- FastBike/ViewController+BikeStationService.swift
+++ FastBike/ViewController+BikeStationService.swift
@@ -4,28 +4,47 @@ extension ViewController {
     func getBikeStations() {
         let url = URL(string: "https://os.smartcommunitylab.it/core.mobility/bikesharing/trento")!
         let session = URLSession(configuration: URLSessionConfiguration.default)
-        let task = session.dataTask(with: url) {
-            data, response, error in
-            if let error = error {
+        let task = session.dataTask(with: url, completionHandler: self.handleResponse)
+        task.resume()
+    }
+
+    private func handleResponse(data: Data?, response: URLResponse?, error: Error?){
+        switch (data, response, error){
+        case let (_, _, error?):
                 print(error.localizedDescription)
-            } else if let httpResponse = response as? HTTPURLResponse {
-                if httpResponse.statusCode == 200 {
-                    if let data = data, let json = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as? [Any] {
-                        if let json = json {
-                            for element in json {
-                                if let element = element as? [String:AnyObject] {
-                                    let bikeStation = BikeStation(json: element)
-                                    self.bikeStations.append(bikeStation)
-                                    DispatchQueue.main.async {
-                                        self.mapView.addAnnotation(bikeStation)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+            
+        case let (data?, response?, _) where self.isSuccess(response: response) :
+            if let jsonData = self.getJson(from: data) {
+                self.populateBikeStations(with: jsonData)
+            }
+            
+        default:
+            print("unhandled case")
+        }
+    }
+
+    private func isSuccess(response: URLResponse) -> Bool {
+        if let httpResponse = response as? HTTPURLResponse {
+            return httpResponse.statusCode == 200
+        }
+        
+        return false
+    }
+
+    private func getJson(from data: Data) -> [[String:AnyObject]]? {
+        if let rawJson = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) {
+            return rawJson as? [[String:AnyObject]]
+        }
+        
+        return .none
+    }
+
+    private func populateBikeStations(with json: [[String:AnyObject]]) {
+        json.map{ BikeStation(json: $0) }.forEach{ bikeStation in
+            self.bikeStations.append(bikeStation)
+            DispatchQueue.main.async {
+                self.mapView.addAnnotation(bikeStation)
             }
         }
-        task.resume()
     }
 }
