fa154971624c144386c032459101bcf5eeb0585e
Tiago Martinho <t.martinho@icloud.com>
2018-10-31 21:47:53 +0100

    Hide report button

--- FastBike/ViewController.swift
+++ FastBike/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         getBikeStations()
         getUserPosition()
         Analytics.track(screen: "Main View")
+        reportButton.isHidden = true
         reportButton.setImage(#imageLiteral(resourceName: "lavoripiena"), for: .highlighted)
         bikeButton.setImage(#imageLiteral(resourceName: "bicipiena"), for: .highlighted)
         parkButton.setImage(#imageLiteral(resourceName: "Parcheggiopiena"), for: .highlighted)
