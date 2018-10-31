970d2e0f5123867969ee41c03b073b3de1653706
Tiago Martinho <t.martinho@icloud.com>
2018-10-31 21:36:33 +0100

    Remove helper function introduced by migrator

--- FastBike/ReportController.swift
+++ FastBike/ReportController.swift
@@ -50,10 +50,7 @@ class ReportController: UITableViewController, UIImagePickerControllerDelegate, 
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-// Local variable inserted by Swift 4.2 migrator.
-let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
-
-        if let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage {
+        if let image = info[.editedImage] as? UIImage {
             photo.image = image
         }
         picker.dismiss(animated: true, completion: nil)
@@ -70,15 +67,4 @@ class TextFieldShouldReturn: NSObject, UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
-	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
-}
-
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-	return input.rawValue
 }
