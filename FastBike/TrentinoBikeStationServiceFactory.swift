c4763be8dd9270af1933ddc3c4dd38dc7d0284bd
Tiago Martinho <t.martinho@icloud.com>
2019-01-16 19:18:24 +0100

    Add remaining trentino cities

--- FastBike/TrentinoBikeStationServiceFactory.swift
+++ FastBike/TrentinoBikeStationServiceFactory.swift
@@ -1,11 +1,19 @@
 import Foundation
 
+// https://www.europeandataportal.eu/data/en/dataset/stazioni-bikesharing-trentino
+
 class TrentinoBikeStationServiceFactory {
 
     private static let endpoints = [
         "lavis",
+        "mezzocorona",
+        "mezzolombardo",
+        "pergine_valsugana",
+        "rovereto",
+        "sanmichelealladige",
         "trento"
     ]
+
     private static let baseEndpoint = "https://os.smartcommunitylab.it/core.mobility/bikesharing/"
 
     static func get() -> [BikeStationService] {
