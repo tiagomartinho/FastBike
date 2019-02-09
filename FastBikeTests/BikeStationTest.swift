1ec34b5dae8fc5cd12076e52e525e21e7aebcdb1
Tiago Martinho <t.martinho@icloud.com>
2019-02-09 16:16:06 +0100

    Add test to verify decode of stations list

--- FastBikeTests/BikeStationTest.swift
+++ FastBikeTests/BikeStationTest.swift
@@ -25,4 +25,41 @@ class BikeStationTest: XCTestCase {
         XCTAssertEqual("Grazioli", station?.name)
         XCTAssertEqual(11.111516155225331, station!.position.last!, accuracy: 0.000000000000001)
     }
+
+    func testDecodeListOfStations() {
+        let jsonString = """
+                        [
+                          {
+                            "name": "Pressano",
+                            "address": "Piazza della Croce - Pressano",
+                            "id": "Pressano - Lavis",
+                            "bikes": 3,
+                            "slots": 4,
+                            "totalSlots": 7,
+                            "position": [
+                              46.15368174037716,
+                              11.106601229430453
+                            ]
+                          },
+                          {
+                            "name": "Stazione RFI",
+                            "address": "Via Stazione - Lavis",
+                            "id": "Stazione RFI - Lavis",
+                            "bikes": 3,
+                            "slots": 7,
+                            "totalSlots": 10,
+                            "position": [
+                              46.148180371138814,
+                              11.096753997622727
+                            ]
+                          }
+                        ]
+                        """
+        let jsonData = jsonString.data(using: .utf8)!
+
+        let stations = try? JSONDecoder().decode([BikeStationNew].self, from: jsonData)
+
+        XCTAssertEqual("Piazza della Croce - Pressano", stations?.first?.address)
+        XCTAssertEqual("Stazione RFI", stations?.last?.name)
+    }
 }
